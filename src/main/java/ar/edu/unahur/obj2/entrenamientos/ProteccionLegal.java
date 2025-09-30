0661612d8f6265bb6b8515a9db1e46a93fcf049c
NicolasUrdiales <nicolas.urdiales@estudiantes.unahur.edu.ar>
2025-09-29 21:59:46 -0300

    test probando decorator

---
+++ src/main/java/ar/edu/unahur/obj2/entrenamientos/ProteccionLegal.java
@@ -0,0 +1,51 @@
+package ar.edu.unahur.obj2.entrenamientos;
+
+import ar.edu.unahur.obj2.profugos.IProfugo;
+
+public class ProteccionLegal extends Entrenamiento{
+
+    public ProteccionLegal(IProfugo profugo) {
+        super(profugo);
+    }
+
+    @Override
+    public Integer getInociencia() {
+        return Integer.max(40, profugo.getInociencia());
+    }
+
+    @Override
+    public Integer getHabilidad() {
+        return profugo.getHabilidad();
+    }
+
+    @Override
+    public Boolean esNervioso() {
+        return profugo.esNervioso();
+    }
+
+    @Override
+    public void volverseNervioso() {
+        profugo.volverseNervioso();
+    }
+
+    @Override
+    public void dejarDeEstarNervioso() {
+        profugo.dejarDeEstarNervioso();
+    }
+
+    @Override
+    public void reducirHabilidad(Integer valor) {
+        profugo.reducirHabilidad(valor);
+    }
+
+    @Override
+    public void disminuirInociencia(Integer valor) {
+        profugo.disminuirInociencia(valor);
+    }
+
+    
+
+
+    
+
+}
--- src/test/java/ar/edu/unahur/obj2/CazadorTest.java
+++ src/test/java/ar/edu/unahur/obj2/CazadorTest.java
@@ -1,12 +1,45 @@
 package ar.edu.unahur.obj2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ar.edu.unahur.obj2.entrenamientos.ArtesMarcialesAvanzadas;
+import ar.edu.unahur.obj2.entrenamientos.EntrenamientoElite;
+import ar.edu.unahur.obj2.entrenamientos.ProteccionLegal;
+import ar.edu.unahur.obj2.profugos.IProfugo;
+import ar.edu.unahur.obj2.profugos.Profugo;
+
 public class CazadorTest {
+    IProfugo profugoComun = new Profugo(20,50,Boolean.FALSE);
+    IProfugo profugoConProteccionLegal = new ProteccionLegal(profugoComun);
+    IProfugo profugoConProteccionLegalYArtesMarciales = new ArtesMarcialesAvanzadas(profugoConProteccionLegal);
+    IProfugo profugoEntrenamientoCompleto = new EntrenamientoElite(profugoConProteccionLegalYArtesMarciales);
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+
     @Test
     void testCazar() {
         assertTrue(Boolean.TRUE);
+    }
+
+    @Test
+    void pruebaProfugo(){
+        assertEquals(40, profugoConProteccionLegalYArtesMarciales.getInociencia());
+        assertEquals(100, profugoConProteccionLegalYArtesMarciales.getHabilidad());
+    }
+    @Test
+    void pruebaProfugo2(){
+        assertEquals(40, profugoEntrenamientoCompleto.getInociencia());
+        assertEquals(100, profugoEntrenamientoCompleto.getHabilidad());
+        profugoEntrenamientoCompleto.volverseNervioso();
+        assertFalse(profugoEntrenamientoCompleto.esNervioso());
+
     }
 }
